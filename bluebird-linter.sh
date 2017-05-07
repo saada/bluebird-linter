01757de2cb22f6bdd09616eedfe9992f3701c80e
Mahmoud Saada <mahmoudsaada@gmail.com>
2017-05-07 15:55:28 -0400

    Create bluebird-linter.sh

---
+++ bluebird-linter.sh
@@ -0,0 +1,17 @@
+echo "Checking if any file uses native Promise without requiring bluebird..."
+FAIL=
+for f in $(grep -R '[^a-zA-Z]Promise' ./{lib,spec/unit} | awk '{print $1}' | sed -e 's/\:.*//' | uniq)
+do
+  if ! grep -iq 'bluebird' "$f"
+  then
+    echo "$f should require('bluebird')" 1>&2
+    FAIL=1
+  fi
+done
+
+if [[ "$FAIL" -eq 1 ]]
+then
+  exit 1
+else
+  echo "Bluebird linting passed successfully"
+fi
